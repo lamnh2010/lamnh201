ab04a4d8d1e0cff746b4a7575824d8063f25d851
lamnh2010 <97387150+lamnh2010@users.noreply.github.com>
2022-02-21 22:22:29 +0700

    lam

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace SandboxProject
     {
       Console.WriteLine("Hello World!");
       char[] alphabet = new char[] {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+      string password = Console.ReadLine();
     }
   }
 } 

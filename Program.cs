fdc2abb5666ec98a17cb8e20260f27ab10cc5125
lamnh2010 <97387150+lamnh2010@users.noreply.github.com>
2022-02-21 14:44:09 +0700

    fix

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace SandboxProject
     static void Main()
     {
       Console.WriteLine("Hello World!");
+      char[] alphabet = new char[] {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
     }
   }
 } 
